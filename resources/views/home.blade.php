e71f48b68c7e629bfaf79f8817bf36e804f38fe3
alejandro sojo <alejandrojsrv@gmail.com>
2019-10-21 21:01:55 -0500

    aa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
             <img src="/assets/img/burguerbeer.png" class="img-fluid pt-3"  style="width:140px;height:140px;" />
             <div class="text-center">
                 <h2 class="mt-4"> Estamos renovando nuestra web</h2>
-                <h4 class="my-4"> Abrimos de Lunes a Viernes <br> hasta las <span class="numero"> 3 </span> am</h4>
+                <h4 class="my-4"> Abrimos de Lunes a Domingo <br> hasta las <span class="numero"> 3 </span> am</h4>
                 <h5 class="mt-4"> 
                     Para pedidos al: 
                     <br>
